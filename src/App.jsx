7e2df8f6ce3119726c7be599932079f85b76450b
alyaanafea <alyaanafea@gmail.com>
2024-06-23 14:42:07 +0300

    code maintain

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,11 @@
-import { useState } from "react";
-import "./App.css";
-import { v4 as uuidv4 } from "uuid";
-import TodoForm from "./components/TodoForm";
-import Todos from "./components/Todos";
-import DoneTodos from "./components/DoneTodos";
+import Container from "./components/Container";
+
+
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [doneTodos, setdoneTodos] = useState([]);
-  const handleAddTodo = (newTodo) => {
-    if (newTodo.trim()) {
-      const id = uuidv4();
-      setTodos([...todos, { todoName: newTodo, id }]);
-    }
-  };
-  const handleDelete = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-    setdoneTodos(doneTodos.filter((todo) => todo.id !== id));
-  };
-  const handleDone = (id) => {
-    const doneTodo = todos.find((todo) => todo.id == id);
-    console.log(doneTodo);
-    setdoneTodos([...doneTodos, doneTodo]);
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
+  
   return (
-    <div className="bg-slate-950 w-full h-screen flex flex-col items-center pt-14 gap-5 text-violet-500">
-      <TodoForm handleAddTodo={handleAddTodo} />
-      <Todos
-        todos={todos}
-        handleDelete={handleDelete}
-        handleDone={handleDone}
-      />
-      <DoneTodos
-        todos={todos}
-        doneTodos={doneTodos}
-        handleDone={handleDone}
-        handleDelete={handleDelete}
-      />
-    </div>
+    <Container/>
   );
 }
 
--- src/components/DoneTodo.jsx
+++ src/components/DoneTodo.jsx
@@ -5,7 +5,7 @@ function DoneTodo({ todo, handleDelete }) {
   const id = todo.id;
   return (
     <div className="flex lg:w-80 w-52 justify-between p-4 bg-gray-900 mb-4 rounded-md">
-      <h2>{todo.todoName}</h2>
+      <h2 className="line-through decoration-green-500">{todo.todoName}</h2>
       <FaTrash onClick={() => handleDelete(id)} />
     </div>
   );
